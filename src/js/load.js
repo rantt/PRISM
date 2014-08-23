c97dd172c3b8dc33eb9ddd6a40ef7cde20e1805a
Dave Canam <david.canam@gmail.com>
2014-08-23 15:03:07 -0300

    Player can walk left/right, and added blue house

--- src/js/load.js
+++ src/js/load.js
@@ -3,20 +3,16 @@ var Game = {
   h: 600
 };
 
-// var w = 800;
-// var h = 600;
-
 Game.Boot = function(game) {
   this.game = game;
 };
 
 Game.Boot.prototype = {
   preload: function() {
-    // console.log('blah'+Game.w);
 		this.game.stage.backgroundColor = '#FFF';
-		this.game.load.images('loading', 'assets/images/loading.png');
-		this.game.load.images('title', 'assets/images/title.png');
-		this.game.load.images('instructions', 'assets/images/instructions.png');
+		this.game.load.image('loading', 'assets/images/loading.png');
+		this.game.load.image('title', 'assets/images/title.png');
+		// this.game.load.image('instructions', 'assets/images/instructions.png');
   },
   create: function() {
    this.game.state.start('Load');
@@ -35,6 +31,10 @@ Game.Load.prototype = {
   	loadingText.anchor.setTo(0.5, 0.5);
   	var preloading = this.game.add.sprite(Game.w/2-64, Game.h/2+50, 'loading');
   	this.game.load.setPreloadSprite(preloading);
+    
+    this.game.load.atlasXML('player','assets/images/player_sheet.png','assets/atlas/player_sheet.xml');
+    this.game.load.tilemap('house','assets/levels/house.json', null, Phaser.Tilemap.TILED_JSON);
+    this.game.load.image('world_blue','assets/images/world_blue.png'); 
 
     // Music Track
     // this.game.load.audio('music','soundtrack.mp3');
--- src/js/play.js
+++ src/js/play.js
@@ -17,6 +17,9 @@ var wKey;
 var aKey;
 var sKey;
 var dKey;
+var cursors;
+var facing = 'right';
+var moveSpeed = 300;
 
 Game.Play = function(game) {
   this.game = game;
@@ -26,10 +29,24 @@ Game.Play.prototype = {
   create: function() {
     this.game.world.setBounds(0, 0 ,Game.w ,Game.h);
 
+    this.player = this.game.add.sprite(Game.w/2,Game.h/2,'player');
+    this.player.anchor.setTo(0.5,0.5);
+
+    this.player.animations.add('right',[3,5,3,7],10,true);
+    this.player.animations.add('left',[4,6,4,8],10,true);
+    this.game.physics.enable(this.player, Phaser.Physics.ARCADE);
+    this.player.body.gravity.y = 750;
+    this.player.body.collideWorldBounds = true;
+    this.game.camera.follow(this.player);
+
+    
+
     // // Music
     // this.music = this.game.add.sound('music');
     // this.music.volume = 0.5;
     // this.music.play('',0,1,true);
+
+    cursors = this.game.input.keyboard.createCursorKeys();
 
     //Setup WASD and extra keys
     wKey = this.game.input.keyboard.addKey(Phaser.Keyboard.W);
@@ -42,6 +59,32 @@ Game.Play.prototype = {
 
   update: function() {
 
+    this.player.body.velocity.x = 0;
+    if(cursors.left.isDown || aKey.isDown) {
+      this.player.body.velocity.x = -moveSpeed;
+      if (facing !== 'left') {
+        this.player.animations.play('left');
+        facing = 'left';
+      }
+    }else if (cursors.right.isDown || dKey.isDown) {
+      this.player.body.velocity.x = moveSpeed;
+      if (facing !== 'right') {
+        this.player.animations.play('right');
+        facing = 'right';
+      }
+    }
+    else {
+      if (facing !== 'idle')
+      {
+        this.player.animations.stop();
+        if (facing === 'left') {
+          this.player.frame = 2;
+        }else{
+          this.player.frame = 1;
+        }
+        facing = 'idle';
+      }
+    }
     // // Toggle Music
     // muteKey.onDown.add(this.toggleMute, this);
 
